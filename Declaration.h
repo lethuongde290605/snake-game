f0395eb7619f40d9c2a180b1a39e5f2014f45530
lethuongde290605 <153986981+lethuongde290605@users.noreply.github.com>
2024-03-12 20:48:30 +0700

    Create Declaration.h

---
+++ Declaration.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <Windows.h>
+#include <conio.h>
+
+
+void FixConsoleWindow();
+void setConsoleWindow(int width, int height);
+void MoveWindow(int posx, int posy);
+void MoveCenter();
+
+//Fix the console window so that the user cannot resize it
+void FixConsoleWindow() {
+	HWND consoleWindow = GetConsoleWindow();
+	LONG style = GetWindowLong(consoleWindow, GWL_STYLE);
+	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
+	SetWindowLong(consoleWindow, GWL_STYLE, style);
+
+}
+
+//Set the console window size
+void setConsoleWindow(int width, int height) {
+	HWND console = GetConsoleWindow();
+	RECT r;
+	GetWindowRect(console, &r);
+	MoveWindow(console, r.left, r.top, width, height, TRUE);
+}
+
+//Move the console window to the specified position
+void MoveWindow(int posx, int posy) 
+{
+    RECT rectClient, rectWindow;
+    HWND hWnd = GetConsoleWindow();
+    GetClientRect(hWnd, &rectClient);
+    GetWindowRect(hWnd, &rectWindow);
+    MoveWindow(hWnd, posx, posy, rectClient.right - rectClient.left, rectClient.bottom - rectClient.top, TRUE);
+}
+
+//Move the console window to the center of the screen
+void MoveCenter()
+{
+    RECT rectClient, rectWindow;
+    HWND hWnd = GetConsoleWindow();
+    GetClientRect(hWnd, &rectClient);
+    GetWindowRect(hWnd, &rectWindow);
+    int posx, posy;
+    posx = GetSystemMetrics(SM_CXSCREEN) / 2 - (rectWindow.right - rectWindow.left) / 2,
+    posy = GetSystemMetrics(SM_CYSCREEN) / 2 - (rectWindow.bottom - rectWindow.top) / 2,
+
+    MoveWindow(hWnd, posx, posy, rectClient.right - rectClient.left, rectClient.bottom - rectClient.top, TRUE);
+}
+
